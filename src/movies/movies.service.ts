cbcd5b13022e78837fbfece68759c57c25fff48f
jeonchaerin <chaerin0411@naver.com>
2022-11-29 16:51:08 +0900

    update: Update movies controller and service

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { NotFoundError } from "rxjs";
 import { Movie } from "./entities/movie.entity";
 
 @Injectable()
@@ -10,11 +11,16 @@ export class MoviesService {
 	}
 
 	getOne(id: string): Movie {
-		return this.movies.find((movie) => movie.id === +id);
+		const movie = this.movies.find((movie) => movie.id === +id);
+		if (!movie) {
+			throw new NotFoundException(`Movie with ID ${id} not found.`);
+		}
+		return movie;
 	}
 
 	deleteOne(id: string): boolean {
-		this.movies.filter((movie) => movie.id !== +id);
+		this.getOne(id);
+		this.movies = this.movies.filter((movie) => movie.id !== +id);
 		return true;
 	}
 
@@ -23,5 +29,11 @@ export class MoviesService {
 			id: this.movies.length + 1,
 			...movieData,
 		});
+	}
+
+	update(id: string, updateData) {
+		const movie = this.getOne(id);
+		this.deleteOne(id);
+		this.movies.push({ ...movie, ...updateData });
 	}
 }
