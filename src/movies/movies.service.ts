3b8de8f3070e63ff464c2638253c86fac25b32ac
jeonchaerin <chaerin0411@naver.com>
2022-11-30 18:18:51 +0900

    feat: Add update-movie.dto.ts and validation

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { NotFoundError } from "rxjs";
 import { CreateMovieDto } from "./dto/create-movie.dto";
+import { UpdateMovieDto } from "./dto/update-movie.dto";
 import { Movie } from "./entities/movie.entity";
 
 @Injectable()
@@ -32,7 +33,7 @@ export class MoviesService {
 		});
 	}
 
-	update(id: number, updateData) {
+	update(id: number, updateData: UpdateMovieDto) {
 		const movie = this.getOne(id);
 		this.deleteOne(id);
 		this.movies.push({ ...movie, ...updateData });
